6e3645f8c400757f23f802178254c3f32b8dd8fd
aberkow <berkowitz.clarinet@gmail.com>
2016-06-05 16:18:26 -0400

    redirect works but should render html instead of json

--- controllers/users.js
+++ controllers/users.js
@@ -86,10 +86,11 @@ exports.default = function(router){
 
     //every time a user is logged in there needs to be a response.
     if (user) {
-      res.status(200).json({
-        message: 'user logged in'
-      });
-      
+      res.redirect('../posts');
+      // res.status(200).json({
+      //   message: 'user logged in'
+      // });
+
     } else {
       return res.status(500).json({
         message: 'server error'
@@ -97,49 +98,3 @@ exports.default = function(router){
     }
   });
 }
-
-
-
-// exports.default = function(router){
-//   //get
-//   router.get('/users', function(req, res){
-//     User.find({}, function(err, users){
-//       if (err) {
-//         return res.status(400).json(err);
-//       }
-//       res.status(200).json(users);
-//     });
-//   });
-//   //create
-//   router.post('/users', function(req, res){
-//     User.create(req.body, function(err, user){
-//       if (err){
-//         res.status(403).json({});
-//         console.error('user not created', err);
-//         return;
-//       }
-//       res.send(user);
-//     });
-//   });
-//   //update
-//   router.put('/users/:id', function(req, res){
-//     User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(err, user){
-//       if (err) {
-//         console.error('User not updated', err);
-//         return;
-//       }
-//       res.status(200).json(user);
-//     });
-//   });
-//   //delete
-//   router.delete('/users/:id', function(req, res){
-//     User.findByIdAndRemove(req.param.id, function(err){
-//       if (err) {
-//         console.error('User not deleted', err);
-//         return;
-//       }
-//       console.log('user deleted');
-//       res.status(200).json({message: 'User deleted'});
-//     });
-//   });
-// }
