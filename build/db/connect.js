97975e3d5c57ef184ce303da3c84f3c02552dd1b
aberkow <berkowitz.clarinet@gmail.com>
2016-06-27 17:49:46 -0400

    error logging

--- build/db/connect.js
+++ build/db/connect.js
@@ -2,8 +2,11 @@ var mongoose = require('mongoose');
 var env = require('../environment');
 var config = require('./config');
 
-// console.log(config, 'config from connect');
-// console.log(env, 'from connect');
-console.log(config[env].url, 'from connect.js');
-//mongoose.connect(config[env].url);
-mongoose.connect('mongodb://test:1234@ds023478.mlab.com:23478/heroku_w01k21nf');
+console.log(config, 'config from connect');
+console.log(env, 'env from connect');
+console.log(config[env].url, 'whole from connect.js');
+mongoose.connect(config[env].url);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+//mongoose.connect('mongodb://test:1234@ds023478.mlab.com:23478/heroku_w01k21nf');
