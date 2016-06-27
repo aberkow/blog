b1d474db541b81ec1a3ade47022997136c41c21c
aberkow <berkowitz.clarinet@gmail.com>
2016-06-26 23:23:17 -0400

    changed config

--- build/db/connect.js
+++ build/db/connect.js
@@ -2,5 +2,6 @@ var mongoose = require('mongoose');
 var env = require('../environment');
 var config = require('./config');
 
+console.log(config, 'config from connect');
 console.log(env, 'from connect');
 mongoose.connect(config[env].url);
