2a28890ab3e763e528c18277021c580875ab0d65
aberkow <berkowitz.clarinet@gmail.com>
2016-06-27 17:13:46 -0400

    trying to work out db login

--- build/db/connect.js
+++ build/db/connect.js
@@ -5,4 +5,5 @@ var config = require('./config');
 // console.log(config, 'config from connect');
 // console.log(env, 'from connect');
 console.log(config[env].url, 'from connect.js');
-mongoose.connect(config[env].url);
+//mongoose.connect(config[env].url);
+mongoose.connect('mongodb://aberkow:qx&YL&#8U3fq@ds023478.mlab.com:23478/heroku_w01k21nf');
