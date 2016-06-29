ba29f429de9e4a0804cf940f8dd2bc20fc0ecdcf
aberkow <berkowitz.clarinet@gmail.com>
2016-06-29 11:34:35 -0400

    exploring connection errors at mongoose.connect

--- build/db/connect.js
+++ build/db/connect.js
@@ -6,7 +6,13 @@ var mongoose = require('mongoose');
 //console.log(env, 'env from connect');
 //console.log(config[env].url, 'whole from connect.js');
 //mongoose.connect(config[env].url);
-mongoose.connect(process.env.MONGODB_URI);
+var mongodb_uri = 'mongodb://adam:adam@ds023478.mlab.com:23478/heroku_w0lk21nf';
+
+mongoose.connect(mongodb_uri, function(error){
+  if (error){
+    console.log('something went wrong in mongoose.connect');
+  };
+});
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
