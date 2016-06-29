00a3533ca5d89a9254cf06feb43436f57d8bb12e
aberkow <berkowitz.clarinet@gmail.com>
2016-06-29 11:20:02 -0400

    trying a different mongodb_URI

--- build/db/connect.js
+++ build/db/connect.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
-//var env = require('../environment');
+var env = require('../environment');
 var config = require('./config');
 
 //console.log(config, 'config from connect');
 //console.log(env, 'env from connect');
 //console.log(config[env].url, 'whole from connect.js');
-//mongoose.connect(config[env].url);
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(config[env].url);
+//mongoose.connect(process.env.MONGODB_URI);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
--- build/environment.js
+++ build/environment.js
@@ -1,1 +1,3 @@
-module.exports = global.environment || process.env.NODE_ENV || process.env.MONGODB_URI || 'development';
+// module.exports = global.environment || process.env.NODE_ENV || process.env.MONGODB_URI || 'development';
+
+module.exports = 'production' || 'localhost';
