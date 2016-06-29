a774b0b5f5a2d9cb3530cb15dcf9d4709a23b5ec
aberkow <berkowitz.clarinet@gmail.com>
2016-06-28 20:41:31 -0400

    trying to use a straightup env variable

--- build/environment.js
+++ build/environment.js
@@ -1,1 +1,1 @@
-module.exports = global.environment || process.env.NODE_ENV || process.env.MONGOLAB_URI || 'development';
+module.exports = global.environment || process.env.NODE_ENV || process.env.MONGODB_URI || 'development';
